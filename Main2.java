5ca0fe30cae1f46e92d56105140a1bfea4bcdf6a
Ian Guedes <114230677+iangpm@users.noreply.github.com>
2023-10-10 09:39:18 -0300

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,13 +1,16 @@
 public class Main2 {
     public static void main (String[] args){
 
-        Livro l = new Livro("Duna", "livro", 123456, 79.9);
+        Livro l = new Livro("Duna", "livro", 123456, 80.00);
         Eletronico e = new Eletronico("PS5", "eletrônico", 12347, 4000);
-        Roupa r = new Roupa("Casaco", "roupa", 215453, 65.9);
+        Roupa r = new Roupa("casaco", "roupa", 215453, 66.00);
 
-        System.out.println("O " + l.getCategoria() + " " + l.getNome() + ", código " + l.getCodigo() + "custa R$" +l.getPreco());
+        System.out.println("O " + l.getCategoria() + " " + l.getNome() + ", código " + l.getCodigo() + ", custa R$" + l.getPreco());
         l.descontar();
-
-
+        System.out.println("O " + e.getCategoria() + " " + e.getNome() + ", de código " + e.getCodigo() + ", custa R$" + e.getPreco());
+        e.descontar();
+        System.out.println("A peça de " + r.getCategoria() + " " + r.getNome() + ", de código " + r.getCodigo() + ", custa R$" + r.getPreco());
+        r.descontar();
+        
     }
 }
